d497270f175c7904de70d65ed3b3380d7cf6ff9f
s-013-28 <s-013-28@gumiforest.ae.krakow.pl>
2023-10-24 14:45:47 +0200

    24-10-23

---
+++ 03-ClassStructure/MyCalendarTest.java
@@ -0,0 +1,4 @@
+public class MyCalendarTest {
+    public static void main(String[] args) {
+    }
+}
---
+++ 03-ClassStructure/Zad5.java
@@ -0,0 +1,9 @@
+public class Zad5{
+    public static void main(String[] args) {
+        
+        System.out.println(Math.max(34,49));
+        System.out.println(Math.PI);
+        System.out.println(Math.abs(-17));
+        System.out.println(Math.sqrt(0));
+    }
+}
