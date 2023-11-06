6fc67d35e217e2f572aa217b9ef182344b66a5ad
BartlomiejRegula <bartek.br2003@gmail.com>
2023-11-06 22:00:01 +0100

    06-11-23

--- 03-ClassStructure/Room.java
+++ 03-ClassStructure/Room.java
@@ -1,3 +1,38 @@
 public class Room {
-    
+    int number;
+    int beds;
+    boolean ocupied;
+    String guestName;
+
+    public Room(int number){
+        this.number = number;
+        this.beds = 2;
+        this.ocupied = false;
+        this.guestName = null;
+    }
+
+    public Room(int number,int beds){
+        this.number = number;
+        this.beds = 2;
+        this.ocupied = true;
+        this.guestName = null;
+    }
+
+    public void checkin(String guestName){
+        this.guestName = guestName;
+        this.ocupied = true;
+    }
+
+    public void checkout(){
+        this.ocupied = false;
+    }
+
+    public boolean isOcupied() {
+        return ocupied;
+    }
+
+    public String displayStatus(){
+        return String.format("room number: %d beds: %d is occupied: %b  guest name: %s", number,beds,ocupied,guestName);
+    }
+
 }
--- 03-ClassStructure/RoomTest.java
+++ 03-ClassStructure/RoomTest.java
@@ -1,3 +1,13 @@
 public class RoomTest {
-    
+    public static void main(String[] args) {
+        Room r = new Room(1);
+        Room[] rooms = new Room[6];
+        // System.out.println(r.displayStatus());
+        // System.out.println(r.number);
+        // System.out.println(r.beds);
+        // System.out.println(r.ocupied);
+        // System.out.println(r.guestName);
+        System.out.println(r.displayStatus());
+        System.out.println(rooms); 
+    }
 }
